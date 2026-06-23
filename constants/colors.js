556563cd8207680d496141ff7a8e6b7f84d93d8b
whodeygus <gustin.puckett@gmail.com>
2026-06-23 04:00:51 +0000

    Add color constants to colors.js

---
+++ constants/colors.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+  green: '#2D5016',
+  greenLight: '#3a6a1f',
+  orange: '#C8641A',
+  navy: '#1A3550',
+  navyLight: '#243f5a',
+  cream: '#f8f6f0',
+  white: '#FFFFFF',
+  black: '#111111',
+  gray: '#666666',
+  grayLight: '#999999',
+  lightGray: '#f0f0f0',
+  border: '#e0dbd0',
+};
